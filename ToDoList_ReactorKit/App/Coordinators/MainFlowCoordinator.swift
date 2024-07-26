e30d8bcc152118ab43f4dea7f99d87315a252911
importants <dlwognsdk@tukorea.ac.kr>
2024-07-26 17:52:04 +0900

    feat: MainFlow > DIContainer / Coordinator 수정

--- ToDoList_ReactorKit/App/Coordinators/MainFlowCoordinator.swift
+++ ToDoList_ReactorKit/App/Coordinators/MainFlowCoordinator.swift
@@ -5,12 +5,16 @@
 //  Created by 이재훈 on 7/25/24.
 //
 
+import RxSwift
+import RxRelay
 import UIKit
 
 final class MainFlowCoordinator: Coordinator {
     private weak var navigationController: UINavigationController?
     private var rootViewController: UIViewController?
     private let dependencies: MainDependencies
+    
+    private let disposeBag = DisposeBag()
     
     init(
         navigationController: UINavigationController?,
@@ -20,20 +24,64 @@ final class MainFlowCoordinator: Coordinator {
         self.dependencies = dependencies
     }
     
+    // start flow
     func start() {
         showMainView()
     }
     
+    deinit {
+        print("MainFlowCoordinator is Deinit")
+    }
+}
+
+// MARK: - Make Flow
+private extension MainFlowCoordinator {
     func showMainView() {
-        let vc = dependencies.makeMainViewController()
+        let vc = dependencies.makeMainViewController(
+            actions: makeMainAction()
+        )
         rootViewController = vc
         
         navigationController?.setViewControllers([vc], animated: true)
     }
     
-    func showDetailView() {
-        let vc = dependencies.makeDetailViewController()
+    func showEditView() {
+        let vc = dependencies.makeEditViewController(
+            actions: makeEditAction()
+        )
         
         navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+
+// MARK: - Make Actions
+private extension MainFlowCoordinator {
+    func makeMainAction(
+    ) -> MainActions {
+        let showEditViewRelay = PublishRelay<Void>()
+        
+        showEditViewRelay
+            .withUnretained(self)
+            .bind { (owner, _) in
+                owner.showEditView()
+            }
+            .disposed(by: disposeBag)
+        
+        return .init(showEditView: showEditViewRelay)
+    }
+    
+    func makeEditAction(
+    ) -> EditActions {
+        let dismissRelay = PublishRelay<Void>()
+        
+        dismissRelay
+            .withUnretained(self)
+            .bind{ (owner, _) in
+                owner.navigationController?.popViewController(animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        return .init(dismiss: dismissRelay)
     }
 }
--- ToDoList_ReactorKit/App/DIContainers/MainDIContainer.swift
+++ ToDoList_ReactorKit/App/DIContainers/MainDIContainer.swift
@@ -8,8 +8,6 @@
 import Foundation
 import UIKit
 
-
-
 protocol MainDIContainerProtocol {
     func makeMainFlowCoordinator(
         navigationController: UINavigationController?
@@ -18,15 +16,19 @@ protocol MainDIContainerProtocol {
 
 protocol MainDependencies {
     func makeMainViewController(
+        actions: MainActions
     ) -> MainViewController
     
-    func makeDetailViewController(
-    ) -> DetailViewController
+    func makeEditViewController(
+        actions: EditActions
+    ) -> EditViewController
 }
 
 final class MainDIContainer: MainDIContainerProtocol, MainDependencies {
+    
     struct Dependencies {
-        
+        // TODO: 공용 서비스 추가
+        let taskService = TaskService()
     }
     
     let dependencies: Dependencies
@@ -46,19 +48,39 @@ final class MainDIContainer: MainDIContainerProtocol, MainDependencies {
         )
     }
     
+    // MARK: - MakeViewController
+    
+    // MARK: MakeMainView
     func makeMainViewController(
+        actions: MainActions
     ) -> MainViewController {
+        
+        let interactor = MainInteractor(service: dependencies.taskService)
+        let router = MainRouter(actions: actions)
+        let reactor = MainViewReactor(
+            interactor: interactor,
+            router: router
+        )
+        
         let vc = MainViewController()
-        let reactor = MainViewReactor()
         vc.reactor = reactor
         
         return vc
     }
     
-    func makeDetailViewController(
-    ) -> DetailViewController {
-        let vc = DetailViewController()
-        let reactor = DetailViewReactor()
+    // MARK: MakeEditView
+    func makeEditViewController(
+        actions: EditActions
+    ) -> EditViewController {
+        
+        let interactor = EditInteractor(service: dependencies.taskService)
+        let router = EditRouter(actions: actions)
+        let reactor = EditViewReactor(
+            interactor: interactor,
+            router: router
+        )
+        
+        let vc = EditViewController()
         vc.reactor = reactor
         
         return vc
